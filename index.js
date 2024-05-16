3043a04a91be007ce9df8943da24c2c34a7fa2c3
Note Asus 2 <cristiano.mcon@gmail.com>
2024-05-16 14:58:16 -0300

    test: requisicao post para criar novo curso

--- index.js
+++ index.js
@@ -1,33 +1,48 @@
 const express = require('express');
-
 const server = express();
+const cursos = ['Programacao','Banco de dados','Front end','PUBG'];
+const portaServer = 3000;
+//node index.js
 
 server.use(express.json());
 
-const cursos = ['Programacao','Banco de dados','Front end'];
+
+//Retorna todos os cursos
+server.get('/cursos',(req,res) => {
+    return res.json(cursos);
+});
 
 //Retorna um curso
 server.get('/cursos/:index',(req,res) => {
     const { index } = req.params;
     return res.json(cursos[index]);
-})
+});
 
-//Retorna todos os cursos
-server.get('/cursos',(req,res) => {
+//Criar um curso
+server.post('/cursos',(req,res) => {    
+    //return res.json(req.body);
+    const { name } = req.body;
+    cursos.push(name);
+    //return res.json(req.body);
     return res.json(cursos);
-})
+});
 
+//Atualizar um curso
+server.put('/cursos/:index',(req,res) => {
+    const { index } = req.params;
+    const { name } = req.body;
+    cursos[index] = name;
+    return res.json(cursos);
+});
 
-
-
-
-
-
-
-
-
-
+//Deletar um curso
+server.delete('/cursos/:index',(req,res) => {
+    const { index } = req.params;
+    cursos.splice(index,1);
+    return res.json({message: "O curso foi deletado"});
+});
 
 
 //porta que sistema fica escutando para retornar
-server.listen(3000);
+server.listen(portaServer);
+//process.exit();
